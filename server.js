3608654c4263203659f5a4ddb1b0f3891d11ef53
vrut57 <vrut57@gmail.com>
2017-05-30 21:47:28 -0400

    gitignore for node_modules folder

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+
+MongoClient.connect('mongodb://admin:admin@ds157631.mlab.com:57631/compound-interest', (err, database) => {
+    if (err) return console.log(err);
+    db = database;
+    app.listen(3000, function () {
+        console.log('listening on 3000');
+    });
+})
+
