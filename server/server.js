6550f4b011d608542969c183f33614202760dc13
vrut57 <vrut57@gmail.com>
2017-07-24 21:47:23 -0400

    basic mongo functionality

--- server/server.js
+++ server/server.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-//Router function queries
-
-
 var router = express.Router();
 
 var db;
@@ -20,11 +17,32 @@ MongoClient.connect('mongodb://admin:admin@ds157631.mlab.com:57631/compound-inte
     });
 })
 
-app.get('/', function(rep, res){
+router.get('/', function (req, res) {
     res.send('hello world!');
+});
+
+router.get('/deposits', function(req, res, next){
+    db.collection('deposits').find(), (err, result) => {
+        if(err) return alert(err);
+        console.log('successful get');
+        res.send(result);
+    };
+});
+
+router.post('/deposits', function (req, res, next){
+    db.collection('deposits').insertOne({
+        user: 'user1',
+        unit: 'year',
+        deposits: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,],
+        attributes: {attr1: 'test1', attr2: 'test2', attr3: 3}
+    }, (error, result) => {
+        assert.equal(null, err);
+        alert('saved to db');
+    })
 });
 
 app.use('/api', router);
 
 
 module.exports = app;
+
